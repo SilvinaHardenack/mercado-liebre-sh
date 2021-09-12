4bdcffe6f26ab5f3d8abffd290b5e83aa4674457
SilvinaHardenack <silvinahardenack@gmail.com>
2021-09-12 20:23:44 -0300

    Listo Heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.use('/', express.static(__dirname + '/public'));
 
-app.listen(3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor funcionando en http://localhost:3000');
 });
 
