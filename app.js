4c812165ebc99edd977e9b7b88fc354a7493bb56
SilvinaHardenack <silvinahardenack@gmail.com>
2021-09-13 09:09:23 -0300

    Correcciones varios archivos app, html y css

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
 
+//Ruta de archivos estáticos//
 app.use('/', express.static(__dirname + '/public'));
 
+//Método listen//
 app.listen(process.env.PORT || 3000, ()=>{
     console.log('Servidor funcionando en http://localhost:3000');
 });
 
+//Envío a la ruta raíz del archivo html//
 app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/views/home.html');
 });
